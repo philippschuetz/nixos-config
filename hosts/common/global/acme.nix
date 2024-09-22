e47c7f45efeff1c1e147e5dd32e383577c774b98
philipp-schuetz <mail@philippschuetz.com>
2024-09-22 21:08:50 +0200

    Update acme.nix

--- hosts/common/global/acme.nix
+++ hosts/common/global/acme.nix
@@ -1,13 +1,6 @@
 {
-  # Enable acme for usage with nginx vhosts
   security.acme = {
     defaults.email = "mail@philippschuetz.com";
     acceptTerms = true;
-  };
-
-  environment.persistence = {
-    "/persist" = {
-      directories = ["/var/lib/acme"];
-    };
   };
 }
