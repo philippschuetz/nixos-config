0e487fd657e85b8d7d02bb0bb4e9120f3aacdaf7
philipp-schuetz <mail@philippschuetz.com>
2024-09-24 18:11:45 +0200

    Add zangendeutsch service

--- hosts/hermes/services/zangendeutsch.nix
+++ hosts/hermes/services/zangendeutsch.nix
@@ -1,1 +1,27 @@
-{}
+let
+  domain = "test.zangendeutsch.de";
+  port = "8001";
+in {
+
+  config = {
+    virtualisation.oci-containers.containers = {
+      "homepage" = {
+        image = "ghcr.io/philipp-schuetz/homepage:main";
+        ports = ["127.0.0.1:${port}:80"];
+      };
+    };
+
+    services.nginx.virtualHosts = {
+      "${domain}" = {
+        serverName = domain;
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${port}";
+        };
+        
+        enableACME = true;
+        forceSSL = true;
+      };
+    };
+  };
+}
