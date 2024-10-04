397385a32b436f76ab1f8afcd464df6685b96e83
philipp-schuetz <mail@philippschuetz.com>
2024-10-04 15:37:08 +0200

    Add config files for athena

---
+++ hosts/athena/default.nix
@@ -0,0 +1,53 @@
+{ pkgs, ... }:
+
+{
+    imports = [
+      ./hardware-configuration.nix
+      ./services
+      ../common/global
+    ];
+
+    networking = {
+      hostName = "athena";
+      useDHCP = true;
+      firewall = {
+        enable = true;
+      };
+      wireless = {
+        enable = true;
+        environmentFile = "../../secrets/wireless.env";
+        networks."@SSID_HOME@".psk = "@PSK_HOME@";
+      };
+    };
+
+    users.users."philipp" = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [
+        vim
+        wget
+        tree
+        git
+      ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9IfjgOwAuY4dh4pOXJBJly4YBjC+LK/4AkpYOnbt0q philipp"
+      ];
+    };
+
+    services.openssh = {
+      enable = true;
+      ports = [ 22 ];
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+      openFirewall = true;
+    };
+
+    programs.nix-ld.enable = true;
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];  
+
+    system.stateVersion = "24.05";
+}
