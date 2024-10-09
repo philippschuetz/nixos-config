8b4470f0c99680e6a00c1b7834b6e0ebd6eb42a6
philipp-schuetz <mail@philippschuetz.com>
2024-10-09 17:46:23 +0200

    Add wildcard cert over dns

--- hosts/common/optional/acme.nix
+++ hosts/common/optional/acme.nix
@@ -1,6 +1,15 @@
-{
+{ pkgs, inputs, config, ... }: {
+  sops.secrets."netcup_dns.env" = { };
   security.acme = {
     defaults.email = "mail@philippschuetz.com";
     acceptTerms = true;
+    certs = {
+      "philippschuetz.com" = {
+        domain = "*.philippschuetz.com";
+        group = config.services.nginx.group;
+        dnsProvider = "netcup";
+        environmentFile = config.sops.secrets."netcup_dns.env".path;
+      };
+    };
   };
 }
