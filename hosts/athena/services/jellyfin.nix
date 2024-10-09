57992c9a239aa1921bf098046d9f0feade6b67bc
philipp-schuetz <mail@philippschuetz.com>
2024-10-09 17:45:57 +0200

    Add jellyfin service to athena

---
+++ hosts/athena/services/jellyfin.nix
@@ -0,0 +1,46 @@
+let
+  domain = "jellyfin.philippschuetz.com";
+  port = 8096;
+in { pkgs, inputs, lib, config, ... }: {
+  services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+      dataDir = "/mnt/ssd-pool/jellyfin";
+    };
+
+    nginx.virtualHosts = {
+      "${domain}" = {
+        serverName = domain;
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${port}";
+        };
+        
+        enableACME = true;
+        forceSSL = true;
+      };
+    };
+  };
+
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = { # hardware.opengl in 24.05
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
+}
