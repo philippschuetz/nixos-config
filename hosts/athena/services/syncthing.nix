da966e02db8f506430a198f8f611001a5812ba5e
philipp-schuetz <mail@philippschuetz.com>
2024-10-14 11:59:31 +0200

    Add syncthing service on athena

---
+++ hosts/athena/services/syncthing.nix
@@ -0,0 +1,73 @@
+let
+  domain = "syncthing.philippschuetz.com";
+  port = 8384;
+in { pkgs, config, ... }: {
+  config = {
+    services.syncthing = {
+      enable = true;
+      configDir = /mnt/ssd-volume/syncthing;
+      dataDir = /mnt/ssd-volume/data;
+      extraFlags = [];
+      group = "syncthing";
+      user = "syncthing";
+      relay.enable = false;
+      systemService = true;
+      openDefaultPorts = true;
+      overrideFolders = true;
+      overrideDevices = true;
+      settings = {
+        folders = {
+          "documents" = {
+            devices = [ "bigbox" ];
+            type = "sendreceive";
+            copyOwnershipFromParent = false;
+            path = "/mnt/ssd-volume/data/documents";
+            enable = true;
+            versioning = {
+              type = "simple";
+              params.keep = "10";
+            };
+          };
+          devices = {
+            "bigbox" = {
+              addresses = [
+                "tcp://192.168.0.10:51820"
+              ];
+              id = "7CFNTQM-IMTJBHJ-3UWRDIU-ZGQJFR6-VCXZ3NB-XUH3KZO-N52ITXR-LAIYUAU";
+              autoAcceptFolders = false;
+            };
+          };
+        };
+        gui = {
+          theme = "black";
+        };
+        options = {
+          urAccepted = -1;
+          relaysEnabled = false;
+          maxFolderConcurrency = 0;
+        };
+      };
+    };
+    services.nginx.virtualHosts = {
+      "${domain}" = {
+        serverName = domain;
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString port}";
+        };
+        
+        useACMEHost = domain;
+        forceSSL = true;
+      };
+    };
+    sops.secrets."netcup_dns.env" = { };
+    security.acme.certs = {
+      "${domain}" = {
+        domain = "${domain}";
+        group = config.services.nginx.group;
+        dnsProvider = "netcup";
+        environmentFile = config.sops.secrets."netcup_dns.env".path;
+      };
+    };
+  };
+}
