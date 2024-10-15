a38ce26fb335a38eeb09895d4206a85c5acf3ba5
philipp-schuetz <mail@philippschuetz.com>
2024-10-15 13:21:39 +0200

    Add user config for philipp

---
+++ hosts/common/users/philipp/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
+  sops.secrets."philipp-password" = {
+    neededForUsers = true;
+  };
+
+  users.mutableUsers = false;
+  users.users."philipp" = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = ifTheyExist [
+      "audio"
+      "network"
+      "video"
+      "wheel"
+    ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9IfjgOwAuY4dh4pOXJBJly4YBjC+LK/4AkpYOnbt0q philipp"
+    ];
+    hashedPasswordFile = config.sops.secrets."philipp-password".path;
+    packages = [pkgs.home-manager];
+  };
+
+  home-manager.users."philipp" = import ../../../../home/philipp/${config.networking.hostName}.nix;
+
+  # security.pam.services = {
+  #   swaylock = {};
+  # };
+}
