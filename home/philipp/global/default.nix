af01107ac802e435dd93f40758ad04b4e86cf79a
philipp-schuetz <mail@philippschuetz.com>
2024-10-16 11:47:55 +0200

    Add hm global for philipp

---
+++ home/philipp/global/default.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  config,
+  outputs,
+  ...
+}: {
+  imports =
+    [ ../features/cli ../features/nvim ];
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      warn-dirty = true;
+    };
+  };
+
+  # systemd.user.startServices = "sd-switch";
+
+  programs.home-manager.enable = true;
+
+  home = {
+    username = lib.mkDefault "philipp";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "24.05";
+    #sessionPath = ["$HOME/.local/bin"];
+    sessionVariables = {
+      FLAKE = "$HOME/coding/nixos-config";
+    };
+  };
+
