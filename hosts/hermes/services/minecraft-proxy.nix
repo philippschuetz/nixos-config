953174b1f845e4d4b0588018907c910f0e74389a
philipp-schuetz <mail@philippschuetz.com>
2024-11-02 22:11:44 +0100

    Fix minecraft proxy

--- hosts/hermes/services/minecraft-proxy.nix
+++ hosts/hermes/services/minecraft-proxy.nix
@@ -2,6 +2,7 @@
   networking.nat = {
     enable = true;
     externalInterface = "ens3";
+    externalIP = "202.61.193.219";
     forwardPorts = [
       {
         destination = "100.104.253.34:25565"; # tailnet ip of athena
