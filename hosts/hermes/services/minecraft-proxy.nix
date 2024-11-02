5bb61c1d3aee6d412c471866c24148089dc9e02f
philipp-schuetz <mail@philippschuetz.com>
2024-11-02 19:27:46 +0100

    Fix minecraft proxy

--- hosts/hermes/services/minecraft-proxy.nix
+++ hosts/hermes/services/minecraft-proxy.nix
@@ -1,10 +1,13 @@
 {
   networking.nat = {
     enable = true;
-    forwardPorts = {
-      destination = "100.104.253.34:25565"; # tailnet ip of athena
-      proto = "tcp";
-      sourcePort = 25565;
-    };
+    forwardPorts = [
+      {
+        destination = "100.104.253.34:25565"; # tailnet ip of athena
+        proto = "tcp";
+        sourcePort = 25565;
+      };
+    ];
   };
+  networking.firewall.allowedTCPPorts = [ 25565 ];
 }
